5ee2a133681f7560e33cbce486dbfb067acb449a
PritishK <91003769+pritishhukayu@users.noreply.github.com>
2024-06-22 11:51:17 +0530

    Create aks.tf

---
+++ aks.tf
@@ -0,0 +1,20 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
